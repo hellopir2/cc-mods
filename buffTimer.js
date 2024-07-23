ff4cf2705ad7143bb10e2d254088c22c2b88ff85
hellopir2 <104927802+hellopir2@users.noreply.github.com>
2024-07-22 22:25:29 -0700

    Create buffTimer.js

---
+++ buffTimer.js
@@ -0,0 +1,16 @@
+eval("Game.gainBuff="+Game.gainBuff.toString().replace(`buff.maxTime=buff.time;`,`buff.maxTime=buff.time;
+buff.desc=buff.desc.split(' for ')[0]+' for '+Game.sayTime(buff.time,-1);
+eval(\`l('buff'+buff.id).onmouseover = function onmouseover(event){\${Game.getTooltip('<div class="prompt" style="min-width:200px;text-align:center;font-size:11px;margin:8px 0px;" id="tooltipBuff"><h3>'+buff.dname+'</h3><div class="line"></div>'+buff.desc+'</div>','left',true).split('"')[3]}}\`);`))
+eval("Game.updateBuffs="+Game.updateBuffs.toString().replace(`buff.time--;`, `buff.time--;
+if(buff.time % 30 < 1){
+    buff.desc=buff.desc.split(' for ')[0]+' for '+Game.sayTime(buff.time,-1);
+    eval(\`l('buff'+buff.id).onmouseover = function onmouseover(event){\${Game.getTooltip('<div class="prompt" style="min-width:200px;text-align:center;font-size:11px;margin:8px 0px;" id="tooltipBuff"><h3>'+buff.dname+'</h3><div class="line"></div>'+buff.desc+'</div>','left',true).split('"')[3]}}\`);
+}`));
+eval("Game.ObjectsById[7].minigame.spells['stretch time'].win="+Game.ObjectsById[7].minigame.spells['stretch time'].win.toString().replace(`me.time+=gain;`,`me.time+=gain;
+me.desc=me.desc.split(' for ')[0]+' for '+Game.sayTime(me.time,-1);
+eval(\`l('buff'+me.id).onmouseover = function onmouseover(event){\${Game.getTooltip('<div class="prompt" style="min-width:200px;text-align:center;font-size:11px;margin:8px 0px;" id="tooltipBuff"><h3>'+me.dname+'</h3><div class="line"></div>'+me.desc+'</div>','left',true).split('"')[3]}}\`);`))
+eval("Game.ObjectsById[7].minigame.spells['stretch time'].fail="+Game.ObjectsById[7].minigame.spells['stretch time'].fail.toString().replace(`me.time=Math.max(me.time,0);`,`me.time=Math.max(me.time,0);
+me.desc=me.desc.split(' for ')[0]+' for '+Game.sayTime(me.time,-1);
+eval(\`l('buff'+me.id).onmouseover = function onmouseover(event){\${Game.getTooltip('<div class="prompt" style="min-width:200px;text-align:center;font-size:11px;margin:8px 0px;" id="tooltipBuff"><h3>'+me.dname+'</h3><div class="line"></div>'+me.desc+'</div>','left',true).split('"')[3]}}\`);`))
+
+// todo: update buff timer while hovering.
