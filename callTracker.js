36a1216963038823e1c179d623d28b25713e83a7
hellopir2 <104927802+hellopir2@users.noreply.github.com>
2024-07-27 19:04:05 -0700

    Create callTracker.js

---
+++ callTracker.js
@@ -0,0 +1,22 @@
+var asdf = Math.seedrandom;
+var rn = document.createElement('span');
+rn.id = "rng";
+rn.style = "padding-left:8px;width:75px;display:inline-block;"
+versionNumber.appendChild(rn);
+var see = document.createElement('span');
+see.id = "curSeed";
+see.style = "padding-left:8px;color:#ff0;"
+versionNumber.appendChild(see)
+Math.seedrandom = (a) => {
+    count = 0;
+    rng.innerHTML = count;
+    (a == undefined)?asdf():asdf(a);
+    curSeed.innerHTML = a;
+    p = Math.random;
+    Math.random = () => {
+        count++;
+        x = p();
+        rng.innerHTML = count;
+        return x;
+    }
+}
