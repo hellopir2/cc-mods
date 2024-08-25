167c1a4421a0f9bceccf190c894d19eb8f53b2dc
hellopir2 <104927802+hellopir2@users.noreply.github.com>
2024-08-25 14:43:38 -0700

    fix stretch time

--- buffTimer.js
+++ buffTimer.js
@@ -5,16 +5,28 @@ eval("Game.gainBuff="+Game.gainBuff.toString().replace(`buff.maxTime=buff.time;`
 buff.desc=buff.desc.split(' for ')[0]+' for '+Game.sayTime(buff.time,-1);
 eval(\`l('buff'+buff.id).onmouseover = function onmouseover(event){\${Game.getDynamicTooltip("(()=>{return Object.values(Game.buffs).filter((buff)=>{return buff.id=="+buff.id+"}).map((buff)=>{return eval(unescape('%27%3Cdiv%20class%3D%22prompt%22%20style%3D%22min-width%3A200px%3Btext-align%3Acenter%3Bfont-size%3A11px%3Bmargin%3A8px%200px%3B%22%20id%3D%22tooltipBuff%22%3E%3Ch3%3E%27+buff.dname+%27%3C/h3%3E%3Cdiv%20class%3D%22line%22%3E%3C/div%3E%27+buff.desc+%27%3C/div%3E%27'));})[0]})",'left',true).split('"')[3]}}\`);`))
 eval("Game.updateBuffs="+Game.updateBuffs.toString().replace(`buff.time--;`, `buff.time--;
+updateStretchTime();
 if(buff.time % 30 < 3){
     buff.desc=buff.desc.split(' for ')[0]+' for '+Game.sayTime(buff.time,-1);
     eval(\`l('buff'+buff.id).onmouseover = function onmouseover(event){\${Game.getDynamicTooltip("(()=>{return Object.values(Game.buffs).filter((buff)=>{return buff.id=="+buff.id+"}).map((buff)=>{return eval(unescape('%27%3Cdiv%20class%3D%22prompt%22%20style%3D%22min-width%3A200px%3Btext-align%3Acenter%3Bfont-size%3A11px%3Bmargin%3A8px%200px%3B%22%20id%3D%22tooltipBuff%22%3E%3Ch3%3E%27+buff.dname+%27%3C/h3%3E%3Cdiv%20class%3D%22line%22%3E%3C/div%3E%27+buff.desc+%27%3C/div%3E%27'));})[0]})",'left',true).split('"')[3]}}\`);
 }`));
-eval("Game.ObjectsById[7].minigame.spells['stretch time'].win="+Game.ObjectsById[7].minigame.spells['stretch time'].win.toString().replace(`me.time+=gain;`,`me.time+=gain;
+
+var updatedStretchTime = false;
+
+function updateStretchTime(){
+    if(updatedStretchTime||!Game.ObjectsById[7].minigameLoaded){
+        return;
+    }
+    eval("Game.ObjectsById[7].minigame.spells['stretch time'].win="+Game.ObjectsById[7].minigame.spells['stretch time'].win.toString().replace(`me.time+=gain;`,`me.time+=gain;
 me.desc=me.desc.split(' for ')[0]+' for '+Game.sayTime(me.time,-1);
 eval(\`l('buff'+me.id).onmouseover = function onmouseover(event){\${Game.getDynamicTooltip("(()=>{return Object.values(Game.buffs).filter((buff)=>{return buff.id=="+me.id+"}).map((buff)=>{return eval(unescape('%27%3Cdiv%20class%3D%22prompt%22%20style%3D%22min-width%3A200px%3Btext-align%3Acenter%3Bfont-size%3A11px%3Bmargin%3A8px%200px%3B%22%20id%3D%22tooltipBuff%22%3E%3Ch3%3E%27+buff.dname+%27%3C/h3%3E%3Cdiv%20class%3D%22line%22%3E%3C/div%3E%27+buff.desc+%27%3C/div%3E%27'));})[0]})",'left',true).split('"')[3]}}\`);`))
-eval("Game.ObjectsById[7].minigame.spells['stretch time'].fail="+Game.ObjectsById[7].minigame.spells['stretch time'].fail.toString().replace(`me.time=Math.max(me.time,0);`,`me.time=Math.max(me.time,0);
+    eval("Game.ObjectsById[7].minigame.spells['stretch time'].fail="+Game.ObjectsById[7].minigame.spells['stretch time'].fail.toString().replace(`me.time=Math.max(me.time,0);`,`me.time=Math.max(me.time,0);
 me.desc=me.desc.split(' for ')[0]+' for '+Game.sayTime(me.time,-1);
 eval(\`l('buff'+me.id).onmouseover = function onmouseover(event){\${Game.getDynamicTooltip("(()=>{return Object.values(Game.buffs).filter((buff)=>{return buff.id=="+me.id+"}).map((buff)=>{return eval(unescape('%27%3Cdiv%20class%3D%22prompt%22%20style%3D%22min-width%3A200px%3Btext-align%3Acenter%3Bfont-size%3A11px%3Bmargin%3A8px%200px%3B%22%20id%3D%22tooltipBuff%22%3E%3Ch3%3E%27+buff.dname+%27%3C/h3%3E%3Cdiv%20class%3D%22line%22%3E%3C/div%3E%27+buff.desc+%27%3C/div%3E%27'));})[0]})",'left',true).split('"')[3]}}\`);`))
+    updatedStretchTime = true;
+}
+
+
 
 // todo: prevent maxTime from updating for cursed finger when using stretch time or buff stacking... EXCEPT THIS MAKES CURSED FINGER CIRCLY THINGY LOOK SILLY >:(
 // todo: replace .desc with a function for all buffs (solves cuf/cookie storm looking different issue)
