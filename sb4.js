ab0be503725093e7dd9f61c70906b5d6909be222
hellopir2 <104927802+hellopir2@users.noreply.github.com>
2024-09-10 21:04:09 -0700

    add tnc+hc+sb4

--- sb4.js
+++ sb4.js
@@ -5,13 +5,41 @@ Game.registerMod("Speed baking IV",{
         this.spritesheet="https://hellopir2.github.io/cc-mods/img/sb.png";
         if (Game.ready) this.createAchievements();
         else Game.registerHook("create", this.createAchievements);
-	Game.Logic = eval('Game.Logic='+Game.Logic.toString().replace("if (timePlayed<=1000*60*15) Game.Win('Speed baking III');", "if (timePlayed<=1000*60*15) Game.Win('Speed baking III');\n\t\t\t\t\tif (timePlayed<=1000*60*5){ if(Game.vault.indexOf(-1)<0) { Game.vault.push(-1); Game.Win('Speed baking IV'); } else { Game.Achievements['Speed baking IV'].won=1; } }\n\t\t\t\t\tif (timePlayed<=1000*60*-5){ if(Game.vault.indexOf(-2)<0) { Game.vault.push(-2); Game.Win('Speed baking V'); } else { Game.Achievements['Speed baking V'].won=1; } }"));
+	Game.Logic = eval('Game.Logic='+Game.Logic.toString().replace("if (timePlayed<=1000*60*15) Game.Win('Speed baking III');",
+`if (timePlayed<=1000*60*15) Game.Win('Speed baking III');
+					if (timePlayed<=1000*60*5) {
+						if(Game.vault.indexOf(-1)<0) {
+							Game.vault.push(-1);
+							Game.Win('Speed baking IV');
+						}
+						else {
+							Game.Achievements['Speed baking IV'].won=1;
+						}
+						if (Game.cookiesEarned>=1000000000 && Game.UpgradesOwned==0 && Game.cookieClicks<=0) {
+							if(Game.vault.indexOf(-3)<0) {
+								Game.vault.push(-3);
+								Game.Win('True Hardcore Speed baking');
+							}
+							else {
+								Game.Achievements['True Hardcore Speed baking'].won=1;
+							}
+						}
+					}
+					if (timePlayed<=1000*60*-5) {
+						if(Game.vault.indexOf(-2)<0) {
+							Game.vault.push(-2); Game.Win('Speed baking V');
+						}
+						else {
+							Game.Achievements['Speed baking V'].won=1;
+						}
+					}`));
         Game.registerHook("check", this.load);
     },
     createAchievements: function(){
         this.achievements = [];
         this.achievements.push(new Game.Achievement("Speed baking IV", "Get to <b>1 million cookies</b> baked in <b>5 minutes</b>.",[0,0,this.spritesheet]));
         this.achievements.push(new Game.Achievement("Speed baking V", "Get to <b>1 million cookies</b> baked in <b>-5 minutes</b>.",[1,0,this.spritesheet]));
+        this.achievements.push(new Game.Achievement("True Hardcore Speed baking", "Get to <b>1 billion cookies</b> baked in <b>5 minutes</b> with <b>no upgrades purchased</b> and <b>no</b> cookie clicks.",[13, 7]));
         for(let i of this.achievements){i.pool="shadow";i.order=30601;}
         LocalizeUpgradesAndAchievs();
     },
